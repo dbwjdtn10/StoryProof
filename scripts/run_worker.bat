aa0b54a99fb3293e31313f2c93ee6e11c76c34cf
dbwjdtn10 <81074350+dbwjdtn10@users.noreply.github.com>
2026-02-20 15:10:14 +0900

    정리: 미사용 파일, 벤치마크 스크립트, Docker 설정 삭제

--- scripts/run_worker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Celery Worker...
-echo Make sure Redis is running! (docker-compose up -d redis)
-
-cd ..
-set PYTHONPATH=%PYTHONPATH%;%CD%
-celery -A backend.worker.celery_app worker --loglevel=info --pool=solo
-pause
